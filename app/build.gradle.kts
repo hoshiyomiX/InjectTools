82d88d17a10d108ec1d9416ec86571878fabf35d
hoshiyomiX <katahiranaruse.daydream@gmail.com>
2026-01-28 01:44:58 +0800

    Add Retrofit and Gson dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    
+    // Networking
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }

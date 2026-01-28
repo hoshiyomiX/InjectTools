cad93a47b242b32b43a518099f4b1210b503cef5
hoshiyomiX <katahiranaruse.daydream@gmail.com>
2026-01-28 16:35:37 +0800

    fix: add material-icons-extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    // Icons Extended for AccountTree, Language, Download, etc.
+    implementation("androidx.compose.material:material-icons-extended")
+    
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     
     // Networking

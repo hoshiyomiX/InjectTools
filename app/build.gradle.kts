b541ef22d42edbcc35714932fce6cdcbadf1d5e4
hoshiyomiX <katahiranaruse.daydream@gmail.com>
2026-01-28 15:16:58 +0800

    revert: restore build.gradle.kts to rollback to 6f296b

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,6 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
@@ -55,6 +50,4 @@ dependencies {
     // Networking
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }

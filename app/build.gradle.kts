32d1f62fc6d9ccfd87ed3a4ad0f9f0851f0097d0
hoshiyomiX <katahiranaruse.daydream@gmail.com>
2026-01-28 15:06:55 +0800

    fix: add missing okhttp dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,11 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
@@ -50,4 +55,6 @@ dependencies {
     // Networking
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
